05abf3f03310d6e381584ab219b18082ca4bde55
okavsar9-boop <okavsar9@gmail.com>
2026-03-20 15:46:47 +0100

    feat: add Pydantic schemas for validation and user service logic

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.models import user, restaurant # This is CRITICAL! 
+from app.routes import user_routes
 
 # Tell SQLAlchemy to create the tables
 Base.metadata.create_all(bind=engine)
