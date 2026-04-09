a81c26ca4b35398dad217fa1b55c614777d97dcc
okavsar9-boop <okavsar9@gmail.com>
2026-04-09 13:51:43 +0200

    feat: implement password hashing using passlib and bcrypt

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,14 +1,18 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user_schema import UserCreate
+from app.core.security import hash_password # Import the tool
 
 def create_user(db: Session, user_data: UserCreate):
-    # In a real app, we would hash the password here!
+    # Hash the password! 
+    hashed_pwd = hash_password(user_data.password)
+    
     new_user = User(
         email=user_data.email,
-        password=user_data.password, # We will add hashing in the next step
+        password=hashed_pwd, # Save the "scrambled" version
         role=user_data.role
     )
+    
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
